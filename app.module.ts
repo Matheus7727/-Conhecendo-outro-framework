4dcda47b70725b20eeb742b776b90c127d9de15f
Matheus Silva Batista <111474396+Matheus7727@users.noreply.github.com>
2023-11-18 17:17:51 -0300

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { LivroListaComponent } from './livro-lista/livro-lista.component';
+import { ControleEditoraService } from './controle-editora.service';
+import { ControleLivrosService } from './controle-livros.service';
+
+@NgModule({
+  declarations: [AppComponent, LivroListaComponent],
+  imports: [BrowserModule],
+  providers: [ControleEditoraService, ControleLivrosService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
